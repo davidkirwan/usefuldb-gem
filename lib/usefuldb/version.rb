3bb6668d3d809b69fe902a7a5e8cf0d5f203d1ba
David Kirwan <00346128@mail.wit.ie>
2012-07-28 21:48:38 +0100

    Creating 0.0.6 release

--- lib/usefuldb/version.rb
+++ lib/usefuldb/version.rb
@@ -7,7 +7,7 @@ module UsefulDB
       
       MAJOR = 0 unless defined? MAJOR
       MINOR = 0 unless defined? MINOR
-      PATCH = 5 unless defined? PATCH
+      PATCH = 6 unless defined? PATCH
 
       def to_s
         [MAJOR, MINOR, PATCH].compact.join('.')
